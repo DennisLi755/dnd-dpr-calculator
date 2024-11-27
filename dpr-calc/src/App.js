02e199c2dfc1061d16d1982bb608145888050891
DennisLi755 <djl6347@g.rit.edu>
2024-11-26 23:04:11 -0500

    dpr calculation complete

--- dpr-calc/src/App.js
+++ dpr-calc/src/App.js
@@ -9,7 +9,11 @@ function App() {
   // for (let i = 0; i < 21; i++) {
   //   data.push(Math.random() * 20);
   // }
+  const AC = Array.apply(null, Array(21)).map((x, idx) => 10 + idx);
 
+  const [atkBonus, setAtkBonus] = useState(0);
+  const [atkNum, setAtkNum] = useState(0);
+  const [critNum, setCritNum] = useState(20);
   const [diceNum, setDiceNum] = useState([0, 0, 0, 0, 0]);
   const [mod, setMod] = useState(0);
   const [data, setData] = useState([]);
@@ -18,10 +22,22 @@ function App() {
   }, [diceNum]);
 
   const calculateDPR = () => {
-    const flatAverage = diceNum.map((num, idx) => num * diceAvg[idx]);
-    //tentative
-    const dpr = flatAverage.reduce((a, b) => a + b) + mod;
+    const avgArray = diceNum.map((num, idx) => num * diceAvg[idx]);
+    const totalAvg = avgArray.reduce((a, b) => a + b);
+    const dpr = AC.map(val => {
+      let C = (21 - critNum) / 20;
+      let P = (21 - val + atkBonus) / 20;
+      let DPH = totalAvg + mod;
+      let DPC = totalAvg;
+      if (P >= 1.0) {
+        P = 0.95;
+      } else if (P <= 0) {
+        P = 0.05;
+      }
+      return ((P * DPH) + (C * DPC)) * atkNum;
+    });
     console.log(dpr);
+    setData(dpr);
   }
 
   return (
@@ -30,6 +46,12 @@ function App() {
       <DamageGraph damageData={data}/>
       <div className="inputs">
         <h3>Input Fields</h3>
+        <label>Attack Bonus</label>
+        <input className="atkBonus" type="number" onChange={e => setAtkBonus(parseInt(e.target.value))} />
+        <label>Num. Attacks</label>
+        <input className="atkBonus" type="number" onChange={e => setAtkNum(parseInt(e.target.value))} />
+        <label>Crit</label>
+        <input className="atkBonus" type="number" value={critNum} onChange={e => setCritNum(parseInt(e.target.value))} />
           {diceTitles.map((title, idx) => (
             <>
               <input className="diceInput" type="number" onChange={e => setDiceNum(diceNum.map((num, id) => id === idx ? num + e.target.value : num))}/>
--- dpr-calc/src/graphs/damage-graph.js
+++ dpr-calc/src/graphs/damage-graph.js
@@ -1,4 +1,4 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 
 // X axis would be AC
 // let's always assume the data array will be for AC's 10-30
@@ -11,11 +11,13 @@ export const DamageGraph = ({damageData}) => {
     });
 
     return (
-        <LineChart width={800} height={500} data={data}>
-        <Line type="monotone" dataKey="Damage" stroke="#8884d8" />
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="AC" />
-        <YAxis />
+        <LineChart width={800} height={500} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <Line type="monotone" dataKey="Damage" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="AC" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
         </LineChart>
     );
 }
