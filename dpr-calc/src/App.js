8affd6842a0444c027d173e9c238ef7d3a3aaee4
DennisLi755 <djl6347@g.rit.edu>
2024-11-26 20:02:35 -0500

    some progress on dice analysis

--- dpr-calc/src/App.js
+++ dpr-calc/src/App.js
@@ -1,22 +1,44 @@
 import {useState, useEffect, useContext} from 'react';
 import './App.css';
 import { DamageGraph } from './graphs/damage-graph';
-import { diceAvg } from './dice-data/dice';
+import { diceAvg, diceTitles } from './dice-data/dice';
 
 function App() {
-  let data = [];
-  for (let i = 0; i < 21; i++) {
-    data.push(Math.random() * 20);
+  //testing stuff
+  // let data = [];
+  // for (let i = 0; i < 21; i++) {
+  //   data.push(Math.random() * 20);
+  // }
+
+  const [diceNum, setDiceNum] = useState([0, 0, 0, 0, 0]);
+  const [mod, setMod] = useState(0);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    //console.log(diceNum);
+  }, [diceNum]);
+
+  const calculateDPR = () => {
+    const flatAverage = diceNum.map((num, idx) => num * diceAvg[idx]);
+    //tentative
+    const dpr = flatAverage.reduce((a, b) => a + b) + mod;
+    console.log(dpr);
   }
-  console.log(diceAvg);
+
   return (
     <>
       <h1>Average DPR from AC 10 - 30</h1>
       <DamageGraph damageData={data}/>
       <div className="inputs">
         <h3>Input Fields</h3>
-          <input className="diceInput" type="text"/>
-          <label>d4</label>
+          {diceTitles.map((title, idx) => (
+            <>
+              <input className="diceInput" type="number" onChange={e => setDiceNum(diceNum.map((num, id) => id === idx ? num + e.target.value : num))}/>
+              <label>{title}</label>
+            </>
+          ))}
+        +
+        <input className="diceInput" type="number" onChange={e => setMod(parseInt(e.target.value))} />
+        <button onClick={calculateDPR}>Calculate DPR</button>
       </div>
     </>
   );
--- dpr-calc/src/dice-data/dice.js
+++ dpr-calc/src/dice-data/dice.js
@@ -11,8 +11,10 @@ const dice = [
     [1, 2, 3, 4, 5, 6, 7, 8],
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-]
+];
+
+const diceTitles = ["d4", "d6", "d8", "d10", "d12"];
 
 const diceAvg = dice.map(die => (die[0] + die[die.length - 1]) / 2);
 
-export {diceAvg, dice};
+export {diceAvg, dice, diceTitles};
